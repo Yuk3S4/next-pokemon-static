41bc650b6a15f937c3a2ed2b68a64be41fa0fb4a
Marco Daniel <marco11.mendia@gmail.com>
2023-04-09 18:56:49 -0600

    fin seccion 5

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    // Los dominios de donde se permitirán extraer imagenes
+    domains: ['raw.githubusercontent.com']
+  }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,46 @@
-import { Button } from '@nextui-org/react'
-import { NextPage } from 'next'
+import { NextPage, GetStaticProps } from 'next'
+import { Grid } from '@nextui-org/react';
 
-const HomePage: NextPage = () => {
+import { pokeApi } from '../api';
+import { PokemonListResponse, SmallPokemon } from '../interfaces';
+import { Layout } from '../components/layouts'
+import { PokemonCard } from '../components/pokemon';
+
+interface Props {
+  pokemons: SmallPokemon[]
+}
+
+// title: Listado de pokemons
+const HomePage: NextPage<Props> = ({ pokemons }) => {
+
   return (
-    <>
-      <Button color="gradient">
-        Hola Mundo
-      </Button>
-    </>
+    <Layout title="Listado de Pokémons">
+      <Grid.Container gap={ 2 } justify='flex-start'>
+        { pokemons.map( poke => (
+          <PokemonCard key={ poke.id } pokemon={ poke } />
+        )) }
+      </Grid.Container>
+    </Layout>
   )
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => { // se ejecuta del lado del servidor y solo en buildtime, sólo se usa en pages. En producción sólo se ejecuta una única vez. Se genera la info antes de que cargue la página
+  
+  const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151')  
+  
+  const pokemons: SmallPokemon[] = data.results.map( (poke, i) => {
+    return {
+      ...poke,
+      id: i + 1,
+      img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${ i + 1 }.svg`
+    }
+  })  
+
+  return {
+    props: { // props - se mandan a las props del componente
+      pokemons
+    }
+  }
 }
 
 export default HomePage
